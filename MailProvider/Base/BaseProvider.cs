cfdd3a9e193f86eaa3104f574802d9bdf3768d97
yenilikci <m.melihcelik017@outlook.com>
2023-05-23 10:44:19 +0300

    mail object model, base provider and smtp provider have been added

---
+++ MailProvider/Base/BaseProvider.cs
@@ -0,0 +1,16 @@
+﻿using MultiThreading.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading.MailProvider.Base
+{
+    public abstract class BaseProvider
+    {
+        public int Port { get; set; } = 587;
+        public string ServerHostName { get; set; } = "smtp.google.com";
+        public abstract Task Send(MailObject mailObject);
+    }
+}
---
+++ MailProvider/SmtpProvider.cs
@@ -0,0 +1,21 @@
+﻿using MultiThreading.MailProvider.Base;
+using MultiThreading.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading.MailProvider
+{
+    public class SmtpProvider : BaseProvider
+    {
+        public override async Task Send(MailObject mailObject)
+        {
+            // Send email
+
+            await Task.Delay(10);
+            LogManager.Default.Log($"SmtpMail has sent to {mailObject.To}");
+        }
+    }
+}
