88b44fa4e3fc9343040fe43a1dbee0a2e27de7d7
yenilikci <m.melihcelik017@outlook.com>
2023-05-23 10:42:47 +0300

    fake data creator has been added

---
+++ FakeDataCreator.cs
@@ -0,0 +1,21 @@
+﻿using MultiThreading.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class FakeDataCreator
+    {
+        public static IEnumerable<MailObject> GetSmtpMails(int number)
+        {
+            Bogus.Faker<MailObject> faker = new Bogus.Faker<MailObject>()
+                .RuleFor(i => i.To, y => y.Internet.Email())
+                .RuleFor(i => i.ProviderType, y => MailProviderType.Smtp);
+
+            return faker.Generate(number);
+        }
+    }
+}
