3e40e93614cdfce5c0f720911b90653b9c069d9b
yenilikci <m.melihcelik017@outlook.com>
2023-05-23 13:50:17 +0300

    mail manager has been added

---
+++ MailManager.cs
@@ -0,0 +1,47 @@
+﻿using MultiThreading.MailProvider;
+using MultiThreading.MailProvider.Base;
+using MultiThreading.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class MailManager
+    {
+        private readonly ConcurrentQueue<MailObject> mailObjects = new ConcurrentQueue<MailObject>();
+        private readonly ConcurrentDictionary<MailProviderType, BaseProvider> MailProviders = new ConcurrentDictionary<MailProviderType, BaseProvider>();
+
+        public void AddMails(IEnumerable<MailObject> mails)
+        {
+            foreach (MailObject mail in mails)
+            {
+                mailObjects.Enqueue(mail);
+            }
+        }
+
+        public async Task SendAllMails()
+        {
+            LogManager.Default.Log("SendAllMails started!");
+            LogManager.Default.Log($"{mailObjects.Count} mails found!");
+
+            var smtpProvider = new SmtpProvider();
+
+            while (mailObjects.Count > 0)
+            {
+                mailObjects.TryDequeue(out MailObject mail);
+                await smtpProvider.Send(mail);
+            }
+
+            LogManager.Default.Log("SendAllMails finished!");
+        }
+
+        private BaseProvider GetProvider (MailProviderType mailProviderType)
+        {
+            return MailProviders.GetOrAdd(mailProviderType, (x) => new SmtpProvider());
+        }
+    }
+}
